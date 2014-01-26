0a8019401c7633baf838292e2675f0a968b6aede
polygonplanet <polygon.planet.aqua@gmail.com>
2014-01-27 02:29:00 +0900

    Update console

---
+++ WSHModule/bin/src/console.js
@@ -0,0 +1,58 @@
+/**
+ * console definition.
+ */
+var console = exports.globals.console = (function() {
+
+  var Console = createObject({ _times: {} });
+
+  mixin(Console, {
+    log: function(x, title) {
+      var args = slice(arguments, 1);
+      var len = args.length;
+      var data;
+
+      if (len > 1 && x.split(/%[%sdj]/).length - 1 === len) {
+        data = format.apply(null, [x].concat(args));
+      } else {
+        data = inspect(x);
+        if (title != null) {
+          data = title + '\n' + data;
+        }
+        data = data.replace(/[\\]{2}/g, '\\').replace(/[\x20]*([}\]])\s*$/, '$1');
+      }
+
+      data = data + '\n';
+
+      //XXX: async
+      WSHModule._postCommand('consolelog', {
+        data: data
+      });
+    },
+    time: function(label) {
+      this._times[label] = Date.now();
+    },
+    timeEnd: function(label) {
+      var time = this._times[label];
+      if (!time) {
+        throw new Error('No such label: ' + label);
+      }
+      var duration = Date.now() - time;
+      this.log('%s: %dms', label, duration);
+    },
+    assert: function(expression) {
+      if (!expression) {
+        //TODO: format message (actual value == expect value)
+        throw new Error(expression);
+      }
+    }
+  });
+
+  //XXX: todo
+  Console.info = Console.warn = Console.error =
+  Console.dir = Console.trace = Console.log;
+
+  return Console;
+}());
+
+exports.console = console;
+
